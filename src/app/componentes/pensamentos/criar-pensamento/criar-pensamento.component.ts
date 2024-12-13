f9ae66093089ec4a9b215e0b27ae619e71ec5fac
felfreitas <soufreitas.felipe@gmail.com>
2024-12-13 19:37:03 -0300

    ajustes no formulario de edição

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -47,5 +47,13 @@ export class CriarPensamentoComponent implements OnInit {
     this.router.navigate(['/listarPensamento'])
   }
 
+  habilitarBotao(){
+    if(this.formulario.valid){
+      return 'botao'
+    }else{
+      return 'botao__desabilitado'
+    }
+  }
+
 
 }
